0438698a3cc0d914a3d45f7caab8da632eaafbe5
thuannnngocc-jpg <thuannnngocc@gmail.com>
2026-07-05 00:18:18 +0700

    Make thread in keep_alive daemon
    Updated the keep_alive function to run the thread as a daemon.

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -11,4 +11,4 @@ def run():
     app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    Thread(target=run).start()
+    Thread(target=run, daemon=True).start()
